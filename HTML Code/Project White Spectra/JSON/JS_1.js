54b9171cecd83b62ce49510c9c3c8fc0528bcbe4
Niko-PL <nikus1ilula@gmail.com>
2024-01-01 19:13:10 +0000

    Programming html
    commiting as there is issue new changes do no affect the code :(

---
+++ HTML Code/Project White Spectra/JSON/JS_1.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+
+function Password_Button() {
+
+    const User_Input = document.getElementById("User_Input");   //address is stored
+    const User_Pass = document.getElementById("User_Pass");     // same
+    User_Pass.innerHTML = User_Intput.value;    //data is outputed to 
+    alert(User_Input.value)
+}
