8f5fedc6188b170ac9371c9aeffa331a01858bce
Niko-PL <nikus1ilula@gmail.com>
2024-01-01 23:09:20 +0000

    More work

--- HTML Code/Project White Spectra/JSON/JS_1.js
+++ HTML Code/Project White Spectra/JSON/JS_1.js
@@ -4,6 +4,8 @@ function Password_Button() {
 
     const User_Input = document.getElementById("User_Input");   //address is stored
     const User_Pass = document.getElementById("User_Pass");     // same
-    User_Pass.innerHTML = User_Intput.value;    //data is outputed to 
+    User_Pass.innerText = User_Intput.value;    //data is outputed to 
     alert(User_Input.value)
 }
+
+
