6a5ee08ec57fd2e5083abfc5c8ae818fc6ae98ee
Niko-PL <nikus1ilula@gmail.com>
2024-03-19 18:08:56 +0000

    forgot to push in ages

--- HTML Code/Project White Spectra/JSON/JS_1.js
+++ HTML Code/Project White Spectra/JSON/JS_1.js
@@ -3,9 +3,29 @@
 function Password_Button() {
 
     const User_Input = document.getElementById("User_Input");   //address is stored
-    const User_Pass = document.getElementById("User_Pass");     // same
-    User_Pass.innerText = User_Intput.value;    //data is outputed to 
-    alert(User_Input.value)
+
+    String String_Input = User_Input
+
+    String_Input = String_Input + "Hi 123"
+
+     document.getElementById("User_Output").innerHTML = String_Input.value;   //address is sent out
+    
+
+
+    alert("done")
 }
 
+/*
+window.onscroll = function () { scroll_navbar() };     //when user scrolls.
 
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function scroll_navbar() {
+    if (window.pageYOffset > sticky) {
+        sidebox.classList.add("sticky");
+    } else {
+        sidebox.classList.remove("sticky");
+    }
+}
+*/
